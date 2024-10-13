e0a86e06eb4643a9fee01d9f110a278173139369
Dana Matthews <dana.delaney.matthews@gmail.com>
2024-10-13 17:38:06 -0400

    Changed name of foreign key status to status_id

--- approot/database.py
+++ approot/database.py
@@ -12,7 +12,8 @@ from sqlalchemy import Boolean, BigInteger, String, DateTime, ForeignKey
 
 from sqlalchemy.orm import (
     Mapped,
-    mapped_column
+    mapped_column,
+    relationship
 )
 
 import library as applib
@@ -38,15 +39,14 @@ class SweatscoreUser(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    status_id: Mapped['UserStatus'] = mapped_column(ForeignKey('user_statuses.id'))
     email_address: Mapped[str] = mapped_column(String(250))
     password: Mapped[str] = mapped_column(String(100))
     created_at: Mapped[datetime] = mapped_column(DateTime)
     updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
 
-    status: Mapped['UserStatus'] = mapped_column(ForeignKey('user_statuses.id'))
 
-
-class ReturnCodes(Base):
+class ReturnCode(Base):
     """ Status codes and descriptions that can be returned to the front-end client """
 
     __tablename__ = 'return_codes'
@@ -56,74 +56,17 @@ class ReturnCodes(Base):
     message: Mapped[str] = mapped_column(String(500))
 
 
-# class FacebookUser(Base):
-#     """ The FacebookUser model/facebook_users table """
+class EmailCode(Base):
+    """
+        Temporary codes that are emailed when establishing a new user account or email address.
+    """
 
-#     __tablename__ = 'facebook_users'
+    __tablename__ = 'email_codes'
 
-#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-#     facebook_id: Mapped[str] = mapped_column(String(50), unique=True)
-#     access_token: Mapped[str] = mapped_column(String(500))
-#     access_token_expiration: Mapped[datetime] = mapped_column(DateTime, nullable=True)
-#     created_at: Mapped[datetime] = mapped_column(DateTime)
-#     updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
-
-#     digiads_user: Mapped["DigiAdsUser"] = relationship(back_populates="facebook_user", cascade="all, delete-orphan")
-
-#     def __repr__(self):
-#         return f"FacebookUser(id={self.id!r}, facebook_id={self.facebook_id!r})"
-
-
-# class DigiAdsUser(Base):
-#     """ The DigiAdsUser model/digiads_users table """
-
-#     __tablename__ = 'digiads_users'
-
-#     id: Mapped["FacebookUser"] = mapped_column(ForeignKey("facebook_users.id"), primary_key=True)
-#     email: Mapped[str] = mapped_column(String(250))
-#     name: Mapped[str] = mapped_column(String(250))
-#     business_name: Mapped[str] = mapped_column(String(250))
-#     phone_number: Mapped[str] = mapped_column(String(25))
-#     website: Mapped[str] = mapped_column(String(250))
-#     active: Mapped[bool] = mapped_column(Boolean(), default=True)
-#     created_at: Mapped[datetime] = mapped_column(DateTime)
-#     updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
-
-#     facebook_user: Mapped["FacebookUser"] = relationship(back_populates="digiads_user")
-#     user_logs: Mapped[list["UserLog"]] = relationship(back_populates="digiads_user", cascade="all, delete-orphan")
-
-#     def __repr__(self):
-#         return f"DigiAdsUser(id={self.id!r}, name={self.name!r})"
-
-
-# class UserLog(Base):
-#     """ The UserLog model/user_logs table """
-
-#     __tablename__ = 'user_logs'
-
-#     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
-#     digiads_user_id: Mapped["DigiAdsUser"] = mapped_column(ForeignKey("digiads_users.id"), index=True)
-#     log_entry: Mapped[str] = mapped_column(String(250))
-#     created_at: Mapped[datetime] = mapped_column(DateTime, index=True)
-
-#     digiads_user: Mapped["DigiAdsUser"] = relationship(back_populates="user_logs")
-
-#     def __repr__(self):
-#         return f"UserLog(user_id={self.user_id!r}, log_entry={self.log_entry!r})"
-
-
-# class FacebookAdAccount(Base):
-#     """ Facebook ad accounts belonging to a user """
-
-#     __tablename__ = 'facebook_ad_accounts'
-
-#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-#     digiads_user_id: Mapped["DigiAdsUser"] = mapped_column(ForeignKey("digiads_users.id"), index=True)
-#     ad_account_id: Mapped[str] = mapped_column(String(30), unique=True)
-#     ad_account_name: Mapped[str] = mapped_column(String(100))
-#     facebook_ad_account_id: Mapped[str] = mapped_column(String(30))
-#     created_at: Mapped[datetime] = mapped_column(DateTime)
-#     updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    code: Mapped[str] = mapped_column(String(8), unique=True)
+    user: Mapped['SweatscoreUser'] = relationship()
 
 
 def get_db_session():
