ee8cdf48c1a9aa60ea4b475482979768e3a7072f
Dana Matthews <dana.delaney.matthews@gmail.com>
2024-10-22 15:40:13 -0400

    Added methods to the authorization library

--- approot/libraries/authorization.py
+++ approot/libraries/authorization.py
@@ -1,6 +1,14 @@
 """ Authorization library functions """
 
 import bcrypt
+import string
+import random
+
+from datetime import datetime, timedelta
+
+from cryptography.fernet import Fernet
+
+import library as applib
 
 def hash_password(unhashed_password):
     """ Takes a plain text password and returns a hashed version of it """
@@ -10,3 +18,43 @@ def hash_password(unhashed_password):
     hashed_password = bcrypt.hashpw(unhashed_password.encode('utf-8'), salt)
 
     return str(hashed_password, 'utf-8')
+
+def get_email_code():
+    """ Generates a code to send to a user's email address """
+
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+
+    return code
+
+def create_session_token(
+        user_id,
+        session_duration = applib.DEFAULT_SESSION_DURATION,
+        session_start = None,
+        secret_key = applib.SWEATSCORE_SECRET_KEY):
+    """ Creates a session, returning the token and expiration timestamp.  """
+
+    # Set the default value for session_start
+    if session_start is None:
+        session_start = datetime.now()
+
+    # Set the expiration time
+    expiration_time = session_start + timedelta(minutes=session_duration)
+
+    expiration_timestamp = expiration_time.timestamp()
+
+    # Encrypt the values
+    value_to_encrypt = f"{user_id}|{expiration_timestamp}"
+
+    fernet = Fernet(bytes(secret_key, encoding='utf-8'))
+
+    encrypted_value = fernet.encrypt(bytes(value_to_encrypt, 'utf-8'))
+
+    # Create a JavaScript compatible time delta. This assumes
+    # the client application knows its own timezone.
+    expiration_timestamp = timedelta(minutes=session_duration).seconds
+    javascript_timedelta = int(expiration_timestamp) * 1000
+
+    return {
+        'token': str(encrypted_value, 'utf-8'),
+        'expiration': javascript_timedelta
+    }
