12a888001459cef33698d552c3a9babe0f364179
Dana Matthews <dana.delaney.matthews@gmail.com>
2024-10-23 01:01:39 -0400

    New code in sign-up router and library

---
+++ approot/apps/signup/library.py
@@ -0,0 +1,158 @@
+"""
+    The signup library. Implements user sign-up and account verification functionality.
+"""
+
+import re
+
+from datetime import datetime, timedelta
+
+from sqlalchemy import select
+
+import library as applib
+
+from database import SweatscoreUser, EmailCode
+
+import libraries.authorization as authlib
+
+
+def new_user(request, dbsession):
+    """
+        Accepts the email and password from a request to create a new user account.
+        Creates a new record in the users table if non exists.
+        Returns an appropriate status code redirecting the user if the account needs to
+        be verified or if the user is already verified.
+    """
+
+    # Check to see if a record already exists in the users table
+    select_query = (
+        select(SweatscoreUser)
+            .where(SweatscoreUser.email_address == request.email)
+    )
+
+    user = dbsession.scalar(select_query)
+
+    if user is None:
+        # Check the password
+        if not validate_password(request.password):
+            return {
+                'code': 'af7hj433',
+                'message': "The chosen password does not meet requirements."
+            }
+
+        # Hash the password if it passes the test
+        password = authlib.hash_password(request.password)
+
+        # Create the user
+        user = SweatscoreUser(
+            email_address = request.email,
+            password = password,
+            created_at = datetime.now(),
+            status_id = 1)
+
+        # Add the user and flush the session to get the new user ID
+        dbsession.add(user)
+
+        dbsession.flush()
+
+        # Generate a sign-up session token
+        session_token = authlib.create_session_token(
+            user.id,
+            applib.SIGNUP_SESSION_DURATION)
+
+        # Add the token to the user record
+        user.session_token = session_token['token']
+
+        dbsession.commit()
+
+        # Create and store the email verification code
+        # email_verification_code = get_email_code()
+
+        # email_code = EmailCode(
+        #     user_id = user.id,
+        #     code = email_verification_code,
+        #     created_at = datetime.now()
+        # )
+
+        # dbsession.add(email_code)
+
+        # dbsession.commit()
+
+        response = {
+            'code': 'i8jhe643',
+            'message': 'The user account has been created.',
+            'session_token': session_token
+        }
+    elif user.status_id == 1:
+        # User email address needs to be verified
+        response = {
+            'code': '84ei1fdf',
+            'message': "User email address needs to be verified."
+        }
+    else:
+        # The user is already signed-up. Redirect to login.
+        response = {
+            'code': '6216ga03',
+            'message': 'The user is already signed-up.'
+        }
+
+    return response
+
+
+def verify_email_address(request, dbsession):
+    """
+        Accepts a code that was sent to a new users email address from the request and
+        verifies it against the code stored in the database.    
+    """
+
+    # Query for the code
+    select_query = (
+        select(EmailCode)
+            .where(EmailCode.code == request.code.upper())
+    )
+
+    code = dbsession.scalar(select_query)
+
+    if code:
+        if code.created_at + timedelta(hours=2) < datetime.now():
+            dbsession.delete(code)
+
+            dbsession.commit()
+
+            return {
+                'code': 'j55j862j',
+                'message': "The verification code does not exist or is expired."
+            }
+        else:
+            code.user.status_id = 2
+
+            dbsession.flush()
+
+            dbsession.delete(code)
+
+            dbsession.commit()
+
+            return {
+                'code': '86ijj0i2',
+                'message': "The users email account is verified."
+            }
+    else:
+        return {
+            'code': 'j55j862j',
+            'message': "The verification code does not exist or is expired."
+        }
+
+
+def validate_password(password):
+    """
+        Validates the password. It should be a certain length
+        and contain the correct combination of characters.
+    """
+
+    regExp = re.compile(r'(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,50}$')
+
+    match = re.match(regExp, password)
+
+    if match:
+        return True
+    else:
+        return False
--- approot/apps/signup/router.py
+++ approot/apps/signup/router.py
@@ -1,32 +1,67 @@
 """
-    The Sign-up router.
+    The signup router.
     Routes requests to the sign-up functions.
 """
 
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from pydantic import (
     BaseModel,
-    Field,
     EmailStr)
+
+from database import get_db_session
+
+from . import library as lib
+
 
 # Models
 
-class NewUser(BaseModel):
-    email: str
+class SessionOut(BaseModel):
+    token: str
+    expiration: int
+
+
+class NewUserIn(BaseModel):
+    email: EmailStr
     password: str
+
+
+class NewUserOut(BaseModel):
+    code: str
+    message: str
+    session_token: SessionOut | None = None
+
+
+class VerifyEmailIn(BaseModel):
+    code: str
+
+
+class VerifyEmailOut(BaseModel):
+    code: str
+    message: str
 
 
 router = APIRouter(prefix='/signup')
 
 
-@router.post('/new-user', response_model_exclude_none=True)
-# def login(request: LoginIn, dbsession: Session=Depends(get_db_session)) -> LoginOut:
-def login(request: NewUser, response: Response) -> str:
+@router.post('/new-user')
+def new_user(request: NewUserIn, dbsession: Session=Depends(get_db_session)) -> NewUserOut:
     """
-        
+        Accepts the email and password from a request to create a new user account.
+        Creates a new record in the users table if non exists.
+        Returns an appropriate status code redirecting the user if the account needs to
+        be verified or if the user is already verified.
     """
 
-    response.status_code = status.HTTP_304_NOT_MODIFIED
-    return "NEW USER"
-    # return lib.login(request, dbsession)
+    return lib.new_user(request, dbsession)
+
+
+@router.post('/verify-email')
+def verify_email_address(request: VerifyEmailIn, dbsession: Session=Depends(get_db_session)) -> VerifyEmailOut:
+    """
+        Accepts a code that was sent to a new users email address from the request and
+        verifies it against the code stored in the database.    
+    """
+
+    return lib.verify_email_address(request, dbsession)
